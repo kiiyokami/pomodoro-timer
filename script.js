ba0d97a4e2cfe7470e44dbf79b0647d528f20def
Kiiyo <hiimkaze@gmail.com>
2023-06-15 22:22:41 +0800

    minor changes and added sound

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 // Variables
 let workTitle = document.getElementById('work');
 let breakTitle = document.getElementById('break');
+let progress = document.querySelector('circle');
+let circleElement = document.getElementById('progressCircle');
 
 
-let workTime = 25;
-let breakTime = 5;
+let workTime = 25*60; // Converted to seconds
+let breakTime = 5 * 60; // Converted to seconds
 
-let seconds = '00';
+// Helper function to format time
+function playSound () {
+	let hector = new Audio('./assets/hector.mp3');
+	hector.play();
+}
 
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+
+  return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+}
 // Displays
 
 console.log(workTitle);
 
 window.onload = () => {
-    document.getElementById('minutes').innerHTML = workTime;
-    document.getElementById('seconds').innerHTML = seconds;
-
+    document.getElementById('timeleft').innerHTML = formatTime(workTime);
     workTitle.classList.add('active');
 }
 
@@ -24,39 +34,32 @@ window.onload = () => {
 function startTimer() {
     document.getElementById('start').style.display = 'none';
     document.getElementById('reset').style.display = 'block';
+  
 
-    seconds = 59;
-
-    let workMinutes = workTime -1;
-    let breakMinutes = breakTime - 1;
+    let workMinutes = workTime;
+    let breakMinutes = breakTime;
 
     breakCount = 0;
 
     setInterval(() => {
-        document.getElementById('minutes').innerHTML = workMinutes;
-        document.getElementById('seconds').innerHTML = seconds;
+        document.getElementById('timeleft').innerHTML = formatTime(workMinutes);
+        workMinutes--;
 
-        seconds--;
-
-        if (seconds === 0){
-            workMinutes--;
             if (workMinutes === -1){
                 if(breakCount % 2 === 0){
                     workMinutes = breakMinutes;
                     breakCount++;
-
+                    playSound();
                     workTitle.classList.remove('active');
                     breakTitle.classList.add('active');
                 }else{
                     workMinutes = workTime;
                     breakCount++;
-
+                    playSound();
                     breakTitle.classList.remove('active');
                     workTitle.classList.add('active');
                 }
             }
-            seconds = 59;
-        }
 
             
         }
